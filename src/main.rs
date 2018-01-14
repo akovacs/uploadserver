6bc4e1b04d4243f4927b99e20092579926a1b4f0
Arpad Kovacs <minhmeoke@gmail.com>
2018-01-13 20:25:19 -0800

    Return GET response as text/html

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@
 
 extern crate rocket;
 
+use rocket::response::content;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> content::Html<&'static str> {
+  content::Html(r#"
+    <!doctype html>
+    <title>Hello World</title>
+    <h1>Hello World!</h1>
+    </html>
+  "#)
 }
 
 fn rocket() -> rocket::Rocket {
