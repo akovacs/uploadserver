56bccb99a120853a0850e8ca5db2c4ffeef5bc82
Arpad Kovacs <minhmeoke@gmail.com>
2018-01-13 20:53:11 -0800

    Add POST endpoint for uploading multipart form data

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,31 @@
 
 extern crate rocket;
 
+use std::io;
+use rocket::Data;
 use rocket::response::content;
+
+#[post("/", format = "multipart/form-data", data = "<data>")]
+fn upload(data: Data) -> io::Result<String> {
+    data.stream_to_file("/tmp/upload.txt").map(|n| n.to_string())
+}
 
 #[get("/")]
 fn index() -> content::Html<&'static str> {
   content::Html(r#"
     <!doctype html>
-    <title>Hello World</title>
-    <h1>Hello World!</h1>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
     </html>
   "#)
 }
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index])
+    rocket::ignite().mount("/", routes![index, upload])
 }
 
 fn main() {
