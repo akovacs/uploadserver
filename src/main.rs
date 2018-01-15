5db215205c07facacde4b869e7357625bc9b9b57
Arpad Kovacs <minhmeoke@gmail.com>
2018-01-14 16:31:50 -0800

    Add logic to parse multipart form data

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate formdata;
 extern crate rocket;
 
 use std::io;
-use rocket::Data;
+use formdata::FormData;
+use rocket::{Data, Outcome, Request};
+use rocket::data::{self, FromData};
+use rocket::http::{HeaderMap, Status};
+use rocket::http::hyper::header::{Headers};
 use rocket::response::content;
 
-#[post("/", format = "multipart/form-data", data = "<data>")]
-fn upload(data: Data) -> io::Result<String> {
-    data.stream_to_file("/tmp/upload.txt").map(|n| n.to_string())
+// Wrap formdata::FormData in order to implement FromData trait
+struct RocketFormData(FormData);
+
+fn from(header_map: &HeaderMap) -> Headers {
+    let mut headers = Headers::new();
+    for header in header_map.iter() {
+        let header_value: Vec<u8> = header.value().as_bytes().to_owned();
+        headers.append_raw(String::from(header.name()), header_value);
+    }
+    headers
+}
+
+impl FromData for RocketFormData {
+    type Error = ();
+
+    fn from_data(request: &Request, data: Data) -> data::Outcome<Self, Self::Error> {
+        let headers = from(request.headers());
+        match formdata::read_formdata(&mut data.open(), &headers) {
+            Ok(parsed_form) => return Outcome::Success(RocketFormData(parsed_form)),
+            _ => return Outcome::Failure((Status::BadRequest, ()))
+        };
+    }
+}
+
+#[post("/", format = "multipart/form-data", data = "<upload>")]
+fn upload(upload: RocketFormData) -> io::Result<String> {
+  for (name, value) in upload.0.fields {
+    println!("Posted field name={} value={}", name, value);
+  }
+  for (name, file) in upload.0.files {
+    println!("Posted file name={} path={:?}", name, file.path);
+    return Ok(format!("Uploaded {}", file.path.to_str().unwrap()));
+  }
+  return Err(io::Error::new(io::ErrorKind::InvalidInput, "No files uploaded"));
 }
 
 #[get("/")]
